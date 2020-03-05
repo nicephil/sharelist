8df049736b0107212f763444f876650333823cf0
nicephil <nicephil@gmail.com>
2020-03-05 22:31:23 +0800

    init

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -tid --name sharelist -p 33001:33001 -v /home/llwang/repos/dockers/sharelist/cache:/sharelist/cache -e HOST=0.0.0.0 -e PORT=33001 oldiy/sharelist:latest
+docker run -tid --restart=always --name sharelist -p 33001:33001 -v /home/llwang/repos/dockers/sharelist/cache:/sharelist/cache -e HOST=0.0.0.0 -e PORT=33001 oldiy/sharelist:latest
